61752de6b0921c9e7e1c7c242da343d32a58a9ca
Ali Madni <ali555m@hotmail.com>
2019-02-11 00:11:09 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,8 @@ def my_collect(array)
   i = 0
   new_array = []
   while i < array.length
-    new_array.push(yeild(array[i]))
+    new_array.push(yeild(array[i])
+    i += 1
   end
   new_array
 end
